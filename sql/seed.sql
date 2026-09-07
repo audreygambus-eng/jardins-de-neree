e6d8eb65bca8d097423dedc11ab23b89c5ed5750
Audrey Gambus <audrey.gambus@gmail.com>
2026-09-07 17:41:45 +0200

    Ajout du jeu de données initial

---
+++ sql/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO tarif (libelle, prix, description, compte_visite, compte_vr) VALUES
+('Billet adulte (18 ans et plus)', 15.00, 'Pour une visite d''une heure de l''aquarium', 1, 0),
+('Billet enfant (de 4 à 18 ans)', 10.00, 'Pour une visite d''une heure de l''aquarium', 1, 0),
+('Billet réduction (étudiants, demandeurs d''emplois, etc)', 12.00, 'Pour une visite d''une heure de l''aquarium * Un justificatif de votre situation vous sera demandé au moment de valider vos billets', 1, 0),
+('Billet groupe (à partir de 6 personnes)', 12.00, 'Pour une visite d''une heure de l''aquarium', 1, 0),
+('Billet VR seule', 8.00, 'Pour une activité VR de 30 minutes sans la visite d''une heure', 0, 1),
+('Billet VR + visite', 20.00, 'Pour une activité VR de 30 minutes avec la visite d''une heure', 1, 1)
+;
+
+INSERT INTO creneau (date_heure, capacite_visite, capacite_vr) VALUES
+('2026-09-12 10:00:00', 60, 8),
+('2026-09-12 11:00:00', 60, 10),
+('2026-09-12 13:00:00', 45, 5),
+('2026-09-12 14:00:00', 15, 2),
+('2026-09-12 15:00:00', 5, 0),
+('2026-09-12 16:00:00', 60, 10),
+('2026-09-12 17:00:00', 60, 10),
+('2026-09-13 10:00:00', 27, 4),
+('2026-09-13 11:00:00', 31, 8),
+('2026-09-13 13:00:00', 22, 10),
+('2026-09-13 14:00:00', 10, 0),
+('2026-09-13 15:00:00', 14, 9),
+('2026-09-13 16:00:00', 55, 7),
+('2026-09-13 17:00:00', 60, 10)
+;
